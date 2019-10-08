c3d878e3896a97707c6ee95a74b93e1488d260f7
StefEmp <stephane.emptage@hotmail.co.uk>
2019-10-08 15:01:20 +0100

    first commit

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    class Weather
+    {
+        public Object getWeatherForecast()
+        {
+            string url = "https://api.edamam.com/search?q=apple&app_id=a4e57804&app_key=5cd9b1cf4de086d7718f9f95c16ccdb7";
+                var client = new WebClient();
+                var content = client.DownloadString(url);
+                var serializer = new JavaScriptSerializer();
+                var jsonContent = serializer.Deserialize<Object>(content);
+                return jsonContent;
+        }
+    }
+}
