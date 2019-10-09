77c4c912de29f0502edb80af34081f6bdce10362
StefEmp <stephane.emptage@hotmail.co.uk>
2019-10-09 14:10:20 +0100

    ingreidents added

--- WeatherApp/Models/Weather.cs
+++ WeatherApp/Models/Weather.cs
@@ -13,11 +13,11 @@ namespace WeatherApp.Models
         public Object getWeatherForecast()
         {
             string url = "https://api.edamam.com/search?q=apple&app_id=a4e57804&app_key=5cd9b1cf4de086d7718f9f95c16ccdb7";
-                var client = new WebClient();
-                var content = client.DownloadString(url);
-                var serializer = new JavaScriptSerializer();
-                var jsonContent = serializer.Deserialize<Object>(content);
-                return jsonContent;
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
         }
     }
 }
